789305e82b65f4d9790ade1aba06bb0559e46d06
ErisanJunior <erisantadeu@gmail.com>
2022-02-20 13:26:40 -0300

    Testando novamente

--- GitHub-CERT/src/main/java/GitHubPack/GitHubCourse.java
+++ GitHub-CERT/src/main/java/GitHubPack/GitHubCourse.java
@@ -5,7 +5,7 @@ public class GitHubCourse {
 	public static void main(String[] args) {
 		AnotherClassToTest outra = new AnotherClassToTest(15.0);
 		
-		System.out.println("Olá GitHub +1!");
+		System.out.println("Olá GitHub !");
 		System.out.println(outra.valorEmString());
 
 	}
